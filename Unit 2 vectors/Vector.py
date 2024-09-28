4cfc1861bde10b8546ff2dd3cdf78b0eb36fed0f
tristanbrewer <tmbrewer@mcdonogh.org>
2024-09-28 17:57:15 -0400

    finished vector class and functions

--- Unit 2 vectors/Vector.py
+++ Unit 2 vectors/Vector.py
@@ -16,25 +16,38 @@ class Vector:
             return Vector(self.x + -1*v.x, self.y + -1*v.y, self.z + -1*v.z)
         raise TypeError(f"Cannot subtract vector with {type(v)}")
     
-    def __mul__(self,v):
-        if not isinstance(v, (float,int)):
+    def __mul__(self,s):
+        if not isinstance(s, (float,int)):
             raise TypeError(f"Cannot multiply vector with {type(v)}, your type is not a scalar")
-        return Vector(self.x * v, self.y * v, self.z * v)
+        return Vector(self.x * s, self.y * s, self.z * s)
     
-    def __truediv__(self,v):
-        if not isinstance(v, (float,int)):
+    def __truediv__(self,s):
+        if not isinstance(s, (float,int)):
             raise TypeError(f"Cannot multiply vector with {type(v)}, your type is not a scalar")
-        return Vector(self.x / v, self.y / v, self.z / v)
+        return Vector(self.x / s, self.y / s, self.z / s)
     
     def findMag(self):
         return(math.sqrt(self.x**2 + self.y**2 + self.z**2))
     
-    def Normalize
+    #dont know if this works
+    def normalize(self):
+        m = self.findMag()
+        return Vector(self.x/m, self.y/m , self.z/m)
     
+    def dotProduct(self,v):
+        if isinstance(v, Vector):
+            raise TypeError(f"Cannot preform the dotproduct {type(v)}, your type is not a vector")
+        return ((self.x*v.x) + (self.y * v.y) + (self.z * v.z))
+        
+    def crossProduct(self,v):
+        newX = (self.y * v.z) - (self.z*v.y)
+        newY = (self.z * v.x) - (self.x*v.z)
+        newZ = (self.x * v.y) - (self.y * v.x)
+        return Vector(newX,newY,newZ)
     def __str__(self):
         return str(self.x) + " "+ str(self.y)+ " " + str(self.z)
     
 Vecc = Vector(2,3,5)
 vic = Vector(1,3,7)
 
-print(Vecc*1)
+print(Vecc.normalize())
