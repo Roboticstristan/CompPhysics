141debba5ea2fa579f8f47e74e608e2f34e56ccc
tristanbrewer <tmbrewer@mcdonogh.org>
2024-10-14 13:01:01 -0400

    added graphics class from mr conn

--- Unit 2 vectors/ProjectileProject/graphics.py
+++ Unit 2 vectors/ProjectileProject/graphics.py
@@ -1,19 +1,37 @@
 #render
+
 import pygame as pg
+import projectileEngine as phys
+
+
 scale=10
 
 def setup(w,h):
+
     global screen
     global clock
     pg.init()
     screen=pg.display.set_mode((w,h))
     pg.display.set_caption("Projectiles")
     clock=pg.time.Clock()
+
 def background():
     screen.fill((200, 225, 250))
     pg.draw.rect(screen, (180, 255, 180), (0, 500, 800, 100))
+
 def render(a):
     background()
     pg.draw.circle(screen,(0,0,0),(scale*a.pos.x,500-1*scale*a.pos.y),5)
     pg.display.flip()
-    clock.tick(60)
+
+def frameRate(a):
+    clock.tick(a)
+
+def check_interactions():
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            phys.run=False
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                phys.go = True        
+
